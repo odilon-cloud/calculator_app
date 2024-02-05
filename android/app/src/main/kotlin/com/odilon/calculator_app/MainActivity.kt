a5ca01f9b9e586df94092a35ffa072c91af164c3
odiloninfra <@odilon-cloud>
2024-02-05 20:48:01 +0200

    adding project

---
+++ android/app/src/main/kotlin/com/odilon/calculator_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.odilon.calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:calculator_app/CalculatorView.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
